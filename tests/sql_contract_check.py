1382140e302c75f6523fd33d06c2d1dcb4a4f970
Nexo Informática <nexoinfo801@gmail.com>
2026-09-03 00:35:55 -0300

    test: chain Fiscal Aprender Delivery enterprise SQLite checks

--- tests/sql_contract_check.py
+++ tests/sql_contract_check.py
@@ -25,3 +25,4 @@ finally:
 
 runpy.run_path(str(root/'tests'/'delivery_pro_sql_check.py'),run_name='__main__')
 runpy.run_path(str(root/'tests'/'intelligence_sql_check.py'),run_name='__main__')
+runpy.run_path(str(root/'tests'/'fiscal_aprender_delivery_sql_check.py'),run_name='__main__')
